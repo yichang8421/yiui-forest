f5dcee29622f1b7b06e05b0f31e58a361f5a1584
andy8421 <yichang8421@qq.com>
2021-12-10 23:13:20 +0800

    添加 aside HTML结构

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -6,7 +6,7 @@
             <h2>一个美观的 UI 框架</h2>
             <p class="actions">
                 <a href="#">GitHub</a>
-                <a href="#">开始</a>
+                <router-link to="/doc">开始</router-link>
             </p>
         </div>
     </div>
