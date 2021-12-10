dcedb081c7071abe9954202e14911b6f8dd2c6d8
andy8421 <yichang8421@qq.com>
2021-12-10 23:04:35 +0800

    Home 组件 css

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,13 +1,14 @@
 <template>
     <div>
         <Topnav/>
-        <div class="banner"></div>
-        <h1>EUI</h1>
-        <h2>一个美观的 UI 框架</h2>
-        <p class="actions">
-            <a href="#">GitHub</a>
-            <a href="#">开始</a>
-        </p>
+        <div class="banner">
+            <h1>EUI</h1>
+            <h2>一个美观的 UI 框架</h2>
+            <p class="actions">
+                <a href="#">GitHub</a>
+                <a href="#">开始</a>
+            </p>
+        </div>
     </div>
 </template>
 
@@ -18,3 +19,29 @@
         components: {Topnav}
     };
 </script>
+
+<style lang="scss" scoped>
+    .banner {
+        padding: 100px 0;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background: #94deca;
+
+        > .actions {
+            padding: 8px 0;
+
+            a {
+                margin: 16px;
+                background: #fff;
+                display: inline-block;
+                $h: 28px;
+                line-height: $h;
+                border-radius: $h/2;
+                padding: 0 8px;
+            }
+        }
+    }
+</style>
