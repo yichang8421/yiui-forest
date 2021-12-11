e596fd79a43c1dc55edcbab00d88b212a1de0304
andy8421 <yichang8421@qq.com>
2021-12-12 00:03:02 +0800

    使用 eventBus 添加父子组件通信

--- src/components/SwitchDemo/index.vue
+++ src/components/SwitchDemo/index.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
-        <Switch/>
+        <Switch :value="selected" @input="selected=$event"/>
     </div>
 </template>
 
 <script lang="ts">
     import Switch from "../../lib/Switch/index.vue";
+    import {ref} from "vue";
 
     export default {
-        components: {Switch}
+        components: {Switch},
+        setup() {
+            const selected = ref(false);
+            return {selected};
+        }
     };
 </script>
--- src/lib/Switch/index.vue
+++ src/lib/Switch/index.vue
@@ -1,19 +1,19 @@
 <template>
-    <button @click="toggle" :class={checked}><span></span></button>
+    <button @click="toggle" :class={checked:value}><span></span></button>
 </template>
 
 <script lang="ts">
-    import {ref} from "vue";
 
     export default {
-        setup() {
-            const checked = ref(false);
+        props: {
+            value: Boolean
+        },
+        setup(props, context) {
             const toggle = () => {
-                checked.value = !checked.value;
-                console.log(checked.value);
+                context.emit("input", !props.value);
             };
 
-            return {checked, toggle};
+            return {toggle};
         }
     };
 </script>
@@ -37,6 +37,7 @@
             width: $h2;
             background: white;
             border-radius: $h2/2;
+            transition: all 250ms; // 动画柔和过渡
         }
 
         &.checked {
