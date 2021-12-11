a63b907a691491e2e357e94348b3f9bb76925869
andy8421 <yichang8421@qq.com>
2021-12-12 00:56:19 +0800

    初始化 Button 组件，使用 slot

--- src/components/ButtonDemo/index.vue
+++ src/components/ButtonDemo/index.vue
@@ -1,3 +1,11 @@
 <template>
-    <div></div>
+    <Button>HELLO</Button>
 </template>
+
+<script lang="ts">
+    import Button from "../../lib/Button/index.vue";
+
+    export default {
+        components: {Button}
+    };
+</script>
--- src/lib/Button/index.vue
+++ src/lib/Button/index.vue
@@ -1,3 +1,5 @@
 <template>
-    <div></div>
+    <button>
+        <slot />
+    </button>
 </template>
