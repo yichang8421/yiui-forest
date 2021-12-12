8661d757f7ab2c63f9a0c182308b2e940ec053a9
andy8421 <yichang8421@qq.com>
2021-12-12 22:27:01 +0800

    固定子组件类型

--- src/lib/Tabs/index.vue
+++ src/lib/Tabs/index.vue
@@ -1,9 +1,6 @@
 <template>
     <div>
         <slot/>
-        <!-- component 渲染一个“元组件”为动态组件。依 is 的值，来决定哪个组件被渲染。is 的值是一个字符串，它既可以是 HTML 标签名称也可以是组件名称。 -->
-        <component :is="defaults[1]"></component>
-        <!-- 可以渲染出来，说明 context.slots.default()[1] 是一个组件 -->
     </div>
 </template>
 
@@ -13,12 +10,11 @@
     export default {
         setup(props, context) {
             const defaults = context.slots.default();
-            // 验证 defaults[0].type === Tab
-            console.log(defaults[0].type);
-            console.log(Tab);
-            if (defaults[0].type === Tab) {
-                console.log("true");
-            }
+            defaults.forEach(tag => {
+                if (tag.type !== Tab) {
+                    throw new Error("Tabs 组件的子组件必须是 Tab");
+                }
+            });
 
             return {defaults};
         }
