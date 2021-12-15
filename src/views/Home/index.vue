4b34a2a5d59b66a48c23d6b730e40ad4ea3c8fef
andy8421 <yichang8421@qq.com>
2021-12-16 00:13:05 +0800

    完成首页样式，页面跳转，响应式布局

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -10,6 +10,55 @@
             </p>
         </div>
     </div>
+    <div class="features">
+        <ul>
+            <li>
+                <a class="selected" href="https://v3.vuejs.org/guide/introduction.html" target="_blank">
+                    <svg>
+                        <use xlink:href="#icon-Vue"></use>
+                    </svg>
+                    <h3>基于 Vue 3</h3>
+                    <p>骄傲地使用了 Vue 3 Composition API</p>
+                </a>
+            </li>
+            <li>
+                <a class="selected" href="https://www.typescriptlang.org/docs/" target="_blank">
+                    <svg>
+                        <use xlink:href="#icon-typescript-def"></use>
+                    </svg>
+                    <h3>基于 TypeScript </h3>
+                    <p>源代码采用 TypeScript 书写（非严格检查）</p>
+                </a>
+            </li>
+            <li>
+                <a href="#" target="_blank">
+                    <svg>
+                        <use xlink:href="#icon-zhongduan"></use>
+                    </svg>
+                    <h3>代码易读</h3>
+                    <p>每个组件的源代码都极其简洁</p>
+                </a>
+            </li>
+            <li>
+                <a class="selected" href="http://en.sass.hk/docs/" target="_blank">
+                    <svg>
+                        <use xlink:href="#icon-scss"></use>
+                    </svg>
+                    <h3>代码易读</h3>
+                    <p>每个组件的源代码都极其简洁</p>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <svg>
+                        <use xlink:href="#icon-food-cookie"></use>
+                    </svg>
+                    <h3>代码易读</h3>
+                    <p>每个组件的源代码都极其简洁</p>
+                </a>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script lang="ts">
@@ -23,18 +72,23 @@
             const switchBtnVisible = () => {
                 (selectBtnVisible as any).value = true;
             };
-            return {switchBtnVisible,selectBtnVisible}
+            return {switchBtnVisible, selectBtnVisible};
         }
     };
 </script>
 
 <style lang="scss" scoped>
+    a:hover {
+        border-bottom: none;
+    }
+
     .yiui-topNavAndBanner {
         background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+        clip-path: ellipse(80% 60% at 50% 40%);
     }
 
     .banner {
-        padding: 100px 0;
+        padding: 110px 0;
 
         display: flex;
         flex-direction: column;
@@ -45,13 +99,76 @@
             padding: 8px 0;
 
             a {
-                margin: 16px;
-                background: #fff;
+                margin: 20px;
+                background: #28afea;
                 display: inline-block;
                 $h: 28px;
                 line-height: $h;
-                border-radius: $h/2;
-                padding: 0 8px;
+                border-radius: 4px;
+                padding: 4px 16px;
+                color: #c9f2ef;
+                transition: 250ms;
+
+                &:hover {
+                    opacity: 70%;
+                    color: white;
+                }
+            }
+        }
+    }
+
+    .features {
+        // 水平居中对齐
+        margin: 64px auto;
+        width: 400px;
+        @media (min-width: 800px) {
+            width: 800px;
+        }
+        @media (min-width: 1200px) {
+            width: 1200px;
+        }
+
+        > ul {
+            display: flex;
+            flex-wrap: wrap;
+
+            > li {
+                display: inline-block;
+
+                > a {
+                    width: 400px;
+                    margin: 16px 0;
+                    display: grid;
+                    justify-content: start;
+                    align-content: space-between;
+                    grid-template-areas:
+                        "icon title"
+                        "icon text";
+                    grid-auto-columns: 80px auto;
+                    grid-auto-rows: 1fr auto;
+
+                    > svg {
+                        display: block;
+                        width: 64px;
+                        height: 64px;
+                        grid-area: icon;
+                    }
+
+                    > h3 {
+                        grid-area: title;
+                        font-size: 28px;
+                    }
+
+                    > p {
+                        grid-area: text
+                    }
+
+                    transition: 150ms;
+
+                    &.selected:hover {
+                        opacity: 60%;
+                    }
+                }
             }
         }
     }
